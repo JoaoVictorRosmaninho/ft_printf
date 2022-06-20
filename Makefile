b8242c7a0f25ef4940d0683167220449196fcdd4
JoaoVictorRosmaninho <joaovictorrosmaninho@gmail.com>
2022-06-19 22:36:02 -0300

    write printf for char, int and char *

---
+++ Makefile
@@ -0,0 +1,39 @@
+SRCS		=	ft_printf.c
+
+OBJS		=	$(SRCS:%.c=%.o)
+
+NAME		=	libftprintf.a
+
+L_PATH		=	libft/
+
+L_NAME		=	libft.a
+
+COMPILE		=	cc
+
+FLAGS		=	-Wall -Wextra -Werror
+
+RM			=	rm -rf
+
+%.o:	%.c
+	@$(COMPILE) $(FLAGS) -c $< -o $(<:%.c=%.o)
+
+all:	$(NAME)
+
+$(NAME): $(OBJS)
+	@ make --no-print-directory -C $(L_PATH)
+	@mv $(L_PATH)/$(L_NAME) $@
+	@ar -rc $(NAME) $(OBJS)
+	@echo "\033[1;92mLibftPrintf		successfully created\033[0m"
+
+clean:
+	@$(RM) $(OBJS)
+	@ make --no-print-directory clean -C $(L_PATH)
+
+fclean:	clean
+	@$(RM) $(NAME)
+	@ make --no-print-directory fclean -C $(L_PATH)
+	@echo "\33[1;93mLibftPrintf	successfully removed\33[0m"
+
+re:	fclean	all
+
+.PHONY:	all	clean	fclean	re
--- ft_printf.c
+++ ft_printf.c
@@ -1,21 +1,51 @@
+#include "./ft_printf.h"
 
-void parser(char token)
+static int parser(char token, va_list items)
 {
+        int len; 
+
+len = 0;
+	if (token == 'd' || token == 'i')
+		len = ft_putnbr_fd(va_arg(items, int), 1);
+	else if (token == 'c')
+		len = ft_putchar_fd(va_arg(items, int), 1);
+	else if (token == '%')
+		len = ft_putchar_fd('%', 1); // +1 aqui 
+	else if (token == 's')
+		len = ft_putstr_fd(va_arg(items, char *), 1);
+/*	else if (token == 'x' || token == 'X')
+		len = ft_print_hex(token, va_arg(items, int));
+	else if (token == 'u')
+		len = ft_print_unint(va_arg(items, unsigned int));
+	else if (token == 'p')
+		len = ft_print_ptr(va_arg(items, unsigned long int)); */ 
+	return (len);
 }
 
 
-int ft_printf(const char *TEMPLATE)
+int ft_printf(const char *TEMPLATE, ...)
 {
-  size_t index;
+      size_t index;
+      va_list arg_list;
+      int len; 
 
-  index = 0;
-  while (TEMPLATE[index])
-  {
-    if (TEMPLATE[index] == '%')
-      parser(TEMPLATE + index + 1);
-    index++;
-  }
-  if (!TEMPLATE)
-    return (0);
+      if (!TEMPLATE)
+        return (0);
+      va_start(arg_list, TEMPLATE); 
+      index = 0;
+      len = 0;
+      while (TEMPLATE[index])
+      {
+        if (TEMPLATE[index] == '%')
+        {
+          len += parser(*(TEMPLATE + index + 1), arg_list);
+          index++;
+        }
+        else 
+          len =+ ft_putchar_fd(TEMPLATE[index], 1); 
+        index++;
+      }
+      va_end(arg_list);
+      return (len);
 }
 

3a2322e7b173fb8139686dfa2e54c03cece3f087
JoaoVictorRosmaninho <joaovictorrosmaninho@gmail.com>
2022-06-26 11:48:41 -0300

    update makefile

--- ft_printf.h
+++ ft_printf.h
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jv <jv@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 23:26:38 by jv                #+#    #+#             */
+/*   Updated: 2022/06/25 23:28:37 by jv               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <stdarg.h>
 # include "./libft/libft.h"
 
-int ft_printf(const char *TEMPLATE, ...); 
-ssize_t ft_printf_hex_lower(int n);
-ssize_t ft_printf_hex_upper(int n);
-ssize_t ft_printf_uint(unsigned int);
-void reverse_array(char *str, int end);
-char *int2hex(unsigned int n, unsigned char op);
-char *lint2hex(unsigned long int n);
-ssize_t ft_printf_ptr(unsigned long int n);
+int		ft_printf(const char *TEMPLATE, ...);
+ssize_t	ft_printf_hex_lower(int n);
+ssize_t	ft_printf_hex_upper(int n);
+ssize_t	ft_printf_uint(unsigned	int n);
+void	reverse_array(char *str, int end);
+char	*int2hex(unsigned int n, unsigned char op);
+char	*lint2hex(unsigned long int n);
+ssize_t	ft_printf_ptr(unsigned long int n);
 #endif
